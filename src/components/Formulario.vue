91e47832d8c994f88d74a7d16efeeb256612e64d
Philipe Barra <philipebarra@gmail.com>
2023-06-30 07:30:50 -0300

    aula: Cronometrando tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -3,6 +3,29 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Formulario",
+  data() {
+    return {
+      tempoEmSegundos: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().slice(11, 19);
+    },
+  },
+  methods: {
+    iniciar() {
+      
+      setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+      console.log('iniciando');
+      
+    },
+    finalizar() {
+      console.log('finalizando');
+    }
+  }
 });
 </script>
 
@@ -25,15 +48,15 @@ export default defineComponent({
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
           <section>
-            <strong>00:00:00</strong>
+            <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
             <span class="icon">
               <i class="fas fa-stop"></i>
             </span>
